4c97ba0dba9c6ca6b536adfb33ad976bf6eaab80
Alex Schimpf <aschimpf1@gmail.coml>
2023-10-02 15:56:04 -0700

    chore: Fixed test

--- examples/simple.py
+++ examples/simple.py
@@ -1,7 +1,7 @@
 # mypy: disable-error-code="no-any-return"
 # flake8: noqa: A003
 
-from typing import List, Any, Dict, TypedDict, Union
+from typing import List, Any, Dict
 from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 
@@ -30,15 +30,13 @@ class ItemV2(BaseModel):
     cost: int
 
 
-class DB(TypedDict):
-    users: Dict[int, Any]
-    items: Dict[int, Any]
+class DB:
+    def __init__(self) -> None:
+        self.users: Dict[int, Any] = {}
+        self.items: Dict[int, Any] = {}
 
 
-db: DB = {
-    'users': {},
-    'items': {}
-}
+db = DB()
 app = FastAPI(
     title='test',
     redoc_url=None
@@ -61,70 +59,70 @@ def get_status() -> str:
 @api_version(1)
 @users_router.get('', deprecated=True)
 def get_users() -> List[User]:
-    return list(db['users'].values())
+    return list(db.users.values())
 
 
 @api_version(1)
 @users_router.post('', deprecated=True)
 def create_user(user: User) -> User:
-    db['users'][user.id] = user
+    db.users[user.id] = user
     return user
 
 
 @api_version(1)
 @users_router.get('/{user_id}', deprecated=True)
 def get_user(user_id: int) -> User:
-    return db['users'][user_id]
+    return db.users[user_id]
 
 
 @api_version(2)
 @users_router.get('')
 def get_users_v2() -> List[UserV2]:
-    return list(user for user in db['users'].values() if isinstance(user, UserV2))
+    return list(user for user in db.users.values() if isinstance(user, UserV2))
 
 
 @api_version(2)
 @users_router.post('')
 def create_user_v2(user: UserV2) -> UserV2:
-    db['users'][user.id] = user
+    db.users[user.id] = user
     return user
 
 
 @api_version(2)
 @users_router.get('/{user_id}')
 def get_user_v2(user_id: int) -> UserV2:
-    return db['users'][user_id]
+    return db.users[user_id]
 
 
 @api_version(1)
 @items_router.get('', deprecated=True)
 def get_items() -> List[Item]:
-    return list(db['items'].values())
+    return list(db.items.values())
 
 
 @api_version(1)
 @items_router.post('', deprecated=True)
 def create_item(item: Item) -> Item:
-    db['items'][item.id] = item
+    db.items[item.id] = item
     return item
 
 
 @api_version(1, remove_in_major=2)
 @items_router.get('/{item_id}', deprecated=True)
 def get_item(item_id: int) -> Item:
-    return db['items'][item_id]
+    return db.items[item_id]
 
 
 @api_version(2)
 @items_router.get('')
 def get_items_v2() -> List[ItemV2]:
-    return list(item for item in db['items'].values() if isinstance(item, ItemV2))
+    return list(item for item in db.items.values() if isinstance(item, ItemV2))
 
 
 @api_version(2)
 @items_router.post('')
 def create_item_v2(item: ItemV2) -> ItemV2:
-    db['items'][item.id] = item
+    db.items[item.id] = item
     return item
 
 
