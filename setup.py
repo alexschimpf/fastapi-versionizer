5014139ed41d0ecdebc232a7d1779eb55d56f2a0
OlegZv <oleg.somov84@gmail.com>
2024-12-17 23:21:15 -0500

    deps: Add 3.13 tests and these will fail

--- setup.py
+++ setup.py
@@ -31,7 +31,9 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'
     ],
     install_requires=requirements_list,
     python_requires='>=3.8'
